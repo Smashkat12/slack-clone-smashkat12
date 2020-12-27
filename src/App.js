fe5bb35c1588487674f0df153b77a3af862cbdf6
Smashkat12 <smashkat12@gmail.com>
2020-12-27 23:26:13 +0200

    header layed out.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="app">
       <h1>This is the start</h1>
+	  {/* Header */}
+	  {/* Sidebar */}
+	  {/* React-Router -> Chat screen */}
     </div>
   );
 }
