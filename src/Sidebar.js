efabb4b93c9000acd2f68c25a13d2d6bd9381367
Smashkat12 <smashkat12@gmail.com>
2020-12-28 01:30:09 +0200

    added material icon to side bar opt

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,6 +11,9 @@ import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
 import AppsIcon from "@material-ui/icons/Apps";
 import FileCopyIcon from "@material-ui/icons/FileCopy";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+
 
 function Sidebar() {
   return (
@@ -33,6 +36,12 @@ function Sidebar() {
       <SidebarOption Icon={AppsIcon} title="Apps" />
       <SidebarOption Icon={FileCopyIcon} title="File browser" />
       <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} title="Channels" />
+
+	  {/* Connect to db and list all the channels */}
     </div>
   );
 }
