d849140d8a6130a99927818f527c27e1de4d5829
Smashkat12 <smashkat12@gmail.com>
2020-12-28 01:01:56 +0200

    added sidebar options with icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,15 @@ import React from "react";
 import "./Sidebar.css";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import SidebarOption from "./SidebarOption";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
 function Sidebar() {
   return (
@@ -14,8 +23,16 @@ function Sidebar() {
             Katlego Morulane
           </h3>
         </div>
-		<CreateIcon />
+        <CreateIcon />
       </div>
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & users" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
     </div>
   );
 }
