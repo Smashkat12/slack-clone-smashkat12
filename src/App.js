2f3f8413cafaf9069d83344d2177d8f2ab5b61c2
Smashkat12 <smashkat12@gmail.com>
2020-12-28 02:34:06 +0200

    added routing for chat display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Chat from "./Chat";
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
-      <Header />
-      <div className="app__body">
-        {/* Sidebar */}
-		<Sidebar />
-        {/* React-Router -> Chat screen */}
-      </div>
+      <Router>
+        {/* Header */}
+        <Header />
+        <div className="app__body">
+          {/* Sidebar */}
+          <Sidebar />
+          {/* React-Router -> Chat screen */}
+          <Switch>
+            <Route path="/room/:roomId">
+              <h1>Chat Screen</h1>
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+              {/* <Chat /> */}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
