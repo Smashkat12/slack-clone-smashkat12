c6fe87efb93871ea4089e69d8cf18add3a245bbe
Smashkat12 <smashkat12@gmail.com>
2021-01-02 11:57:25 +0200

    added styling and fixed timestamp display

--- src/Message.js
+++ src/Message.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import './Message.css'
+import React from "react";
+import "./Message.css";
 
 function Message({ timestamp, message, user, userImage }) {
-	return (
-		<div className='message'>
-			<img src={userImage} alt=""/>
-			<div className="message__info">
-				<h4>
-					{user} timestamp...
-				</h4>
-				<p>{message}</p>
-			</div>
-			
-		</div>
-	)
+  return (
+    <div className="message">
+      <img src={userImage} alt="" />
+      <div className="message__info">
+        <h4>
+          {user}{" "}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
+        </h4>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Message
+export default Message;
