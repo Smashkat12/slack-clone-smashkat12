43eae2c8970e2dfdf5c6ed82f9a13aae10f7d375
Smashkat12 <smashkat12@gmail.com>
2021-01-02 15:18:59 +0200

    added chat input and linked chats to db

---
+++ src/ChatInput.js
@@ -0,0 +1,43 @@
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import "./ChatInput.css";
+import { db } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import firebase from "firebase";
+
+function ChatInput({ channelName, channelId }) {
+  /* keep track of what the user is typing */
+  const [input, setInput] = useState("");
+  const [{ user }] = useStateValue();
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (channelId) {
+      db.collection("rooms").doc(channelId).collection("messages").add({
+        message: input,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        user: user.displayName,
+        userImage: user.photoURL,
+      });
+    }
+  };
+
+  
+  return (
+    <div className="chatInput">
+      <form>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder={`Message #${channelName?.toLowerCase()}`}
+        />
+        <Button type="submit" onClick={sendMessage}>
+          SEND
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default ChatInput;
