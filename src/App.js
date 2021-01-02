e1ed0d6a69a2d7367c7dc5b12c4461cf6d005b29
Smashkat12 <smashkat12@gmail.com>
2021-01-02 12:52:37 +0200

    added login comp with styling

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Chat from "./Chat";
+import { useState } from "react";
+import Login from "./Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
       <Router>
-        {/* Header */}
-        <Header />
-        <div className="app__body">
-          {/* Sidebar */}
-          <Sidebar />
-          {/* React-Router -> Chat screen */}
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome</h1>
-              {/* <Chat /> */}
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              {/* Sidebar */}
+              <Sidebar />
+              {/* React-Router -> Chat screen */}
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome</h1>
+                  {/* <Chat /> */}
+                </Route>
+              </Switch>
+            </div>{" "}
+          </>
+        )}
       </Router>
     </div>
   );
---
+++ src/Login.js
@@ -0,0 +1,33 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+import { auth, provider } from "./firebase";
+
+function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://static.toiimg.com/thumb/72975551.cms?width=680&height=512&imgsize=881753"
+          alt=""
+        />
+        <h1>Sign in to Slack</h1>
+        <p>smash.slack.com</p>
+        <Button onClick={signIn}>Sign In with Google</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
